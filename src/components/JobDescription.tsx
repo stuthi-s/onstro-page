228891ddff5466f972421be08ce8929a3270cf73
stuthi-s <stuthisanil@gmail.com>
2025-03-28 09:44:34 +0530

    ninth-changes

---
+++ src/components/JobDescription.tsx
@@ -0,0 +1,15 @@
+"use client";
+import DOMPurify from "dompurify";
+import { Job } from "@/types/career";
+
+const JobDescription = ({ job }: { job: Job }) => {
+const sanitizedDescription = DOMPurify.sanitize(job?.description || "");
+
+return (
+<div className="max-w-none text-gray-800 leading-relaxed text-lg mt-6">
+<div dangerouslySetInnerHTML={{ __html: sanitizedDescription }} />
+</div>
+)
+}
+
+export default JobDescription
